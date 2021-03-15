f59cfa08515f18e46c18b4beef12dfa6d6f77a88
Stephanie Harrison <stephanzonator@hotmail.com>
2021-03-15 22:47:00 +0000

    functionality to create new URLs added. Reflects on URL display page when this happens

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+const morgan = require('morgan');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let result = Math.random().toString(36).replace('0.', '');
+  return result.slice(0,6);
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,7 +32,13 @@ app.get("/set", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+  
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
@@ -38,6 +56,12 @@ app.get("/urls", (req, res) => {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  urlDatabase[generateRandomString()] = req.body["longURL"];
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
