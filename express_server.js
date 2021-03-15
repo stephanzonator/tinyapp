f6799e5503d5704286076fda8d0482bd20e4d0fa
Stephanie Harrison <stephanzonator@hotmail.com>
2021-03-15 21:10:29 +0000

    set and fetch added

--- express_server.js
+++ express_server.js
@@ -7,6 +7,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
